c248e9ed007060974fead5bc98be55c9c7d759e9
fularani <ranivhansure@gmail.com>
2022-12-12 10:40:20 +0530

    adminjs plugin connected to prisma

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... your Prisma Client queries will go here
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: {
+          title: 'Hello World',
+        },
+      },
+    },
+  })
+  console.log('Created new user: ', newUser)
+
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  console.log('All users: ')
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => await prisma.$disconnect())
